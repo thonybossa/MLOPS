f532765ec6a076bf50dbfbac7cde9632248cc72a
thonybossa <thony.aabo@gmail.com>
2024-04-10 20:49:57 -0500

    update

--- Proyecto_2/dags/main.py
+++ Proyecto_2/dags/main.py
@@ -140,7 +140,7 @@ def pipeline():
     start = DummyOperator(task_id='start')
     prev_task = start
 
-    for i in range(1): # aca se cambia el parametro para el numero de batch
+    for i in range(10): # aca se cambia el parametro para el numero de batch
         load_task = load_data(i)
         prev_task = prev_task >> load_task
 
