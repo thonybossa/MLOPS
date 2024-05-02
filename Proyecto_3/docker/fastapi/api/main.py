8c8a571231ca1f2e3dac737e59772ba434acb6a6
delahozvictorandres@gmail.com <personal>
2024-05-02 17:50:11 -0500

    inference + api sql connection

--- Proyecto_3/docker/fastapi/api/main.py
+++ Proyecto_3/docker/fastapi/api/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
-from api.schemas import CoverType
+from api.schemas import PatientData
+from sqlalchemy import create_engine, inspect
 import pandas as pd
 import os
 import mlflow
@@ -9,7 +10,6 @@ os.environ['MLFLOW_S3_ENDPOINT_URL'] = "http://minio:9000"
 os.environ['AWS_ACCESS_KEY_ID'] = 'minioadmin'
 os.environ['AWS_SECRET_ACCESS_KEY'] = 'minioadmin'
 mlflow.set_tracking_uri("http://mlflow:8083")
-
 
 model_name = "best_model"
 model_version = 1
@@ -23,41 +23,76 @@ app = FastAPI()
 # Define endpoint for root URL
 @app.get("/")
 async def root():
-    return {"message": "Welcome to the CoverType Prediction API!"}
+    return {"message": "Welcome to the Readmitted Prediction API!"}
 
 
 # Define endpoint for prediction
 @app.post("/predict")
-async def predict_cover_type(data: CoverType):
+async def predict_readmitted(data: PatientData):
     # Perform prediction
     prediction = model.predict(
         [
-            [
-                data.race,
-                data.gender,
-                data.age,
-                data.admission_type_id,
-                data.discharge_disposition_id,
-                data.admission_source_id,
-                data.time_in_hospital,
-                data.medical_specialty,
-                data.num_lab_procedures,
-                data.num_procedures,
-                data.num_medications,
-                data.number_emergency,
-                data.number_inpatient,
-                data.diag_1,
-                data.diag_2,
-                data.diag_3,
-                data.number_diagnoses,
-                data.insulin,
-                data.change,
-                data.diabetesMed,
-            ]
+            {
+                'race': data.race,
+                'gender': data.gender,
+                'age': data.age,
+                'admission_type_id': data.admission_type_id,
+                'discharge_disposition_id': data.discharge_disposition_id,
+                'admission_source_id': data.admission_source_id,
+                'time_in_hospital': data.time_in_hospital,
+                'medical_specialty': data.medical_specialty,
+                'num_lab_procedures': data.num_lab_procedures,
+                'num_procedures': data.num_procedures,
+                'num_medications': data.num_medications,
+                'number_emergency': data.number_emergency,
+                'number_inpatient': data.number_inpatient,
+                'diag_1': data.diag_1,
+                'diag_2': data.diag_2,
+                'diag_3': data.diag_3,
+                'number_diagnoses': data.number_diagnoses,
+                'insulin': data.insulin,
+                'change': data.change,
+                'diabetesMed': data.diabetesMed,
+            }
         ]
     )
     # Convertir el resultado de la predicción a un diccionario
     prediction_dict = {
         "prediction": str(prediction[0])
     }  # Convertir la predicción a una cadena
+    
+    # Crear un diccionario con los datos
+    datos = {
+        'race': [data.race],
+        'gender': [data.gender],
+        'age': [data.age],
+        'admission_type_id': [data.admission_type_id],
+        'discharge_disposition_id': [data.discharge_disposition_id],
+        'admission_source_id': [data.admission_source_id],
+        'time_in_hospital': [data.time_in_hospital],
+        'medical_specialty': [data.medical_specialty],
+        'num_lab_procedures': [data.num_lab_procedures],
+        'num_procedures': [data.num_procedures],
+        'num_medications': [data.num_medications],
+        'number_emergency': [data.number_emergency],
+        'number_inpatient': [data.number_inpatient],
+        'diag_1': [data.diag_1],
+        'diag_2': [data.diag_2],
+        'diag_3': [data.diag_3],
+        'number_diagnoses': [data.number_diagnoses],
+        'insulin': [data.insulin],
+        'change': [data.change],
+        'diabetesMed': [data.diabetesMed],
+        'prediction': prediction[0]
+    }
+
+    # Convertir el diccionario en un DataFrame
+    df = pd.DataFrame(datos)
+    engine = create_engine('mysql+mysqlconnector://ab:ab@mysql/Base_de_Datos')
+    inspector = inspect(engine)
+    if inspector.has_table('fastapi_predictions'):
+        df.to_sql('fastapi_predictions', engine, if_exists='append', index=False)
+    else:
+        df.to_sql('fastapi_predictions', engine, if_exists='fail', index=False)
+    
     return prediction_dict
